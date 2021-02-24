e351589570fab1099e41360440539fd56f2f7bbe
Maria Garcia <rhian+maria@jumpingrivers.com>
2021-02-24 15:37:02 +0000

    Make scatter pink

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(col = "firebrick") +
+  geom_point(col = "pink") +
   labs(caption = "Copyright by the Australian Commonwealth Bureau of Meteorology")
 
 # Save the plot in graphics/
